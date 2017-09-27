1f48ac9137726d031b4d75df80a2061e4586520a
Maxime Poulain <poulain@et.esiea.fr>
2017-09-27 11:27:12 +0200

    new version of exo_dom

--- Lesson2/exo_dom_lesson_2.py
+++ Lesson2/exo_dom_lesson_2.py
@@ -15,21 +15,14 @@ def getValues(url):
     dictionnaire = {}
     for i in soup.find_all('tr',class_='bleu'):
         element = i.find('td',class_='libellepetit G')
-        if element != None and re.search(r'= A$',element.text):
-             dictionnaire['A'] = [(int(x.text.split('\xa0')[0].replace(' ',''))) for x in i.find_all('td','montantpetit G')[1:3]]
-        if element != None and re.search(r'= B$',element.text):
-            dictionnaire['B'] = [(int(x.text.split('\xa0')[0].replace(' ',''))) for x in i.find_all('td','montantpetit G')[1:3]]
-        if element != None and re.search(r'= C$',element.text):
-            dictionnaire['C'] = [(int(x.text.split('\xa0')[0].replace(' ',''))) for x in i.find_all('td','montantpetit G')[1:3]]
-        if element != None and re.search(r'= D$',element.text):
-            dictionnaire['D'] = [(int(x.text.split('\xa0')[0].replace(' ',''))) for x in i.find_all('td','montantpetit G')[1:3]]
+        if element != None and re.search(r'= [A-D]$',element.string):
+            dictionnaire[element.text.split(' ')[-1]] = [(int(x.string.split('\xa0')[0].replace(' ',''))) for x in i.find_all('td','montantpetit G')[1:3]]
     return dictionnaire
-
-
-
 
 final_dict = {}
 for annee in range(2010,2016):
     url = 'http://alize2.finances.gouv.fr/communes/eneuro/detail.php?icom=056&dep=075&type=BPS&param=5&exercice='+str(annee)
     final_dict[annee] = getValues(url)
-print(final_dict)
+    print('Année '+str(annee))
+    print(final_dict[annee])
+    print('---------')
